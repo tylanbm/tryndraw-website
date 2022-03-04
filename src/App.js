4c1a08c938f505fb7719e771107599d4f128d4fa
tylanbm <tylanmcpherson@gmail.com>
2022-03-03 21:01:02 -0800

    First commit -> Outlined website

--- src/App.js
+++ src/App.js
@@ -4,20 +4,48 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        
       </header>
+      <body>
+        <h1>
+          What is TRYNDRAW?
+        </h1>
+        
+        <p>
+          TRYNDRAW is a lite social media platform, enabling users to draw out hilarious senarios for their friends and the world to see!
+        </p>
+
+        <h1>
+          Image slideshow
+        </h1>
+
+        <h1>
+          View on github button
+        </h1>
+
+        <h1>
+          Try Now with Expo GO!
+
+          QR CODE
+        </h1>
+
+        <h1>
+          See it in action!
+
+          VIDEO
+        </h1>
+
+        <h1>
+          Meet the creators!
+
+          Tylan, Tyrell cards(links and photos)
+        </h1>
+
+
+
+
+      </body>
     </div>
   );
 }
