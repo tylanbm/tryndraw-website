1f3b256ef470e4efd5ac146fd0b5dc966c90ae9a
tylanbm <tylanmcpherson@gmail.com>
2022-03-16 12:36:24 -0700

    Sectioned main container into sperarate ones

--- src/App.js
+++ src/App.js
@@ -14,45 +14,58 @@ function App() {
         <img src={require('../src/logo.png')} alt="TRYNDRAW logo" width={168} />
       </header>
       
-      <div className="main-container">
-        <body>
-            
-          {/* Slide deck that displays images to showcase app features */}
-          <Slideshow />
-
-          <h1 className='heading-container'>
-            What is TRYNDRAW?
-          </h1>
+      <div className='flow spacing'>
+        <body> 
           
+          {/* Container for info section */}
+          <div className='main-container flow'>
+            {/* Slide deck that displays images to showcase app features */}
+            <Slideshow />
 
-          <p className='text-container'>
-            TRYNDRAW is a lite social media platform, enabling users to draw out hilarious senarios for their friends and the world to see!
-          </p>
-          
+            <h1 className='heading-container heading'>
+              What is TRYNDRAW?
+            </h1>
+
+            <p className='text-container text'>
+              TRYNDRAW is a lite social media platform, enabling users to draw out hilarious senarios for their friends and the world to see!
+            </p>
+
+            <div className='button-width'>
+              <MainButton label='View On Github' link="https://github.com/tylanbm/TRYNDRAW" />
+            </div>
+          </div>
+  
           <div className='spacing'></div>
 
-          <div className='button-width'>
-            <MainButton label='View On Github' link="https://github.com/tylanbm/TRYNDRAW" />
+          {/* Container for qr code */}
+          <div className='container-bg'>
+            <div className='main-container flow'>
+              <div>
+                <img className='qr-code' src={require('../src/frame.png')} alt="QR code for Expo GO link" />
+              </div>
+            
+              <h1>
+              Try Now with Expo GO!
+
+              QR CODE
+              </h1>
+            </div>
+          </div>
+          
+          {/* Container for video demo */}
+    
+          <div className='main-container flow'>
+            <h1 className='left'>
+              See it in action!
+            </h1>
+            <YoutubeEmbed embedId="rokGy0huYEA" />
           </div>
 
+          {/* Container for creator info */}
+          <div className='container-bg spacing'>
+          <div className='main-container flow'>
           
-          
-
-          <div >
-            <img className='qr-code' src={require('../src/frame.png')} alt="QR code for Expo GO link" />
-          </div>
-          <h1>
-            Try Now with Expo GO!
-
-            QR CODE
-          </h1>
-
-          <h1>
-            See it in action!
-          </h1>
-          <YoutubeEmbed embedId="rokGy0huYEA" />
-
-          <h1>
+          <h1 className='left'>
             Meet the team behind TRYNDRAW
           </h1>
           
@@ -60,6 +73,11 @@ function App() {
             <AuthorCard source="tylan.jpg" label='View Tylan on Github' link='https://github.com/tylanbm' />
             <AuthorCard source="tyrell.jpg" label='View Tyrell on Github' link='https://github.com/Natty444'  />
           </div>
+          </div>
+          </div>
+          
+
+          
           
         </body>
         
