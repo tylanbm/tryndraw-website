0aeb22b3072d83fbb1b648c45df14a39066620fd
tylanbm <tylanmcpherson@gmail.com>
2022-04-06 16:39:35 -0700

    Updated links, wrapped buttons in anchor

--- src/App.js
+++ src/App.js
@@ -1,109 +1,115 @@
-import logo from './logo.svg';
-import React from 'react';
+import React from "react";
 import YoutubeEmbed from "./components/YoutubeEmbed";
-import './App.css';
-import './styles.css';
-import MainButton from './components/MainButton';
-import AuthorCard from './components/AuthorCard';
-import Slideshow from './components/Slideshow';
-import Version from './components/Version';
+import "./App.css";
+import "./styles.css";
+import MainButton from "./components/MainButton";
+import AuthorCard from "./components/AuthorCard";
+import Slideshow from "./components/Slideshow";
+import Version from "./components/Version";
 
 function App() {
   return (
-    <div className=''>
-      <header className='header-container'>
-        <img src={require('../src/logo.png')} alt="TRYNDRAW logo" width={168} />
+    <div className="">
+      <header className="header-container">
+        <img src={require("../src/logo.png")} alt="TRYNDRAW logo" width={168} />
       </header>
-      
-      <div className='flow spacing'>
-        <body> 
-          
+
+      <div className="flow spacing">
+        <body>
           {/* Container for info section */}
-          <div className='main-container flow'>
+          <div className="main-container flow">
             {/* Slide deck that displays images to showcase app features */}
             <Slideshow />
 
-            <h1 className='heading-container heading'>
-              What is TRYNDRAW?
-            </h1>
+            <h1 className="heading-container heading">What is TRYNDRAW?</h1>
 
-            <p className='text-container text'>
-              TRYNDRAW is a lite social media platform, enabling users to draw out hilarious scenarios for their friends and the world to see!
+            <p className="text-container text">
+              TRYNDRAW is a lite social media platform, enabling users to draw
+              out hilarious scenarios for their friends and the world to see!
             </p>
 
-            <div className='spacing16'></div>
+            <div className="spacing16"></div>
 
-            <div className='button-width center'>
-              <div className='half-width'>
-                <MainButton label='View On Github' link="https://github.com/tylanbm/TRYNDRAW" />
+            <div className="button-width center">
+              <div className="half-width">
+                <MainButton
+                  label="View On Github"
+                  link="https://github.com/tylanbm/TRYNDRAW"
+                />
               </div>
-              
             </div>
           </div>
-  
-          <div className='spacing'></div>
+
+          <div className="spacing"></div>
 
           {/* Container for qr code */}
-          <div className='container-bg'>
-            <div className='main-container flow'>
-            <h1 className='heading'>
-              Try now on Android with Expo GO!
-              </h1>
-              <div className='text-container'>
-              <p className='text'>
-                If you have an android device you can scan the QR code below to get started. Not currently available on IOS devices.
-              </p>
+          <div className="container-bg">
+            <div className="main-container flow">
+              <h1 className="heading">Try now on Android with Expo GO!</h1>
+              <div className="text-container">
+                <p className="text">
+                  If you have an android device you can scan the QR code below
+                  to get started. Not currently available on IOS devices.
+                </p>
               </div>
-              
-              
-              <div className='center'>
-              <div className='half-width'>
-                <img className='qr-code' src={require('../src/frame.png')} alt="QR code for Expo GO link" />
-                <Version label={'Scan with Expo Go on Android'} icon="android" />
-                <Version label={'Coming soon to IOS'} icon="apple" />
+
+              <div className="center">
+                <div className="half-width">
+                  <img
+                    className="qr-code"
+                    src={require("../src/frame.png")}
+                    alt="QR code for Expo GO link"
+                  />
+                  <Version
+                    label={"Scan with Expo Go on Android"}
+                    icon="android"
+                  />
+                  <Version label={"Coming soon to IOS"} icon="apple" />
+                </div>
               </div>
-              </div>
-              
-            
-              
             </div>
           </div>
 
-          <div className='spacing'></div>
-          
-          {/* Container for video demo */}
-          <div className='main-container flow'>
-            <h1 className='heading'>
-              See it in action!
-            </h1>
-            <YoutubeEmbed embedId="rokGy0huYEA" />
-          </div>
+          <div className="spacing"></div>
 
+          {/* Container for video demo */}
+          <div className="main-container flow">
+            <h1 className="heading">See it in action!</h1>
+            <YoutubeEmbed embedId="iKMtFTNSjQQ" />
+          </div>
 
           {/* Container for creator info */}
-          <div className='spacing'></div>
-          <div className='main-container flow'>
-          
-          <h1 className='heading'>
-            Meet the team behind TRYNDRAW
-          </h1>
-          
-          <div className='authors'>
-            <AuthorCard source="tylan.jpg" label='View Tylan on Github' link='https://github.com/tylanbm' />
-            <AuthorCard source="tyrell.jpg" label='View Tyrell on Github' link='https://github.com/Natty444'  />
+          <div className="spacing"></div>
+          <div className="main-container flow">
+            <h1 className="heading">Meet the team behind TRYNDRAW</h1>
+
+            <div className="authors">
+              <AuthorCard
+                source="tylan.jpg"
+                label="View Tylan on Github"
+                link="https://github.com/tylanbm"
+              />
+              <AuthorCard
+                source="tyrell.jpg"
+                label="View Tyrell on Github"
+                link="https://github.com/tyrellbain4"
+              />
+            </div>
           </div>
-          </div>
-          
-          
-        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-        
+
+          <script
+            type="module"
+            src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+          ></script>
+          <script
+            nomodule
+            src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+          ></script>
         </body>
-        
       </div>
-      <footer className='footer'>
-          <p className='footer-text'>©2022 Tylan Bayley-McPherson</p>
-        </footer>
+      <footer className="footer">
+        <p className="footer-text">©2022 Tylan Bayley-McPherson</p>
+      </footer>
     </div>
   );
 }
--- src/components/MainButton.js
+++ src/components/MainButton.js
@@ -2,17 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const MainButton = ({ label, link }) => (
-  
-  <div className="button-container">
-    <a href={link}>
+  <a href={link}>
+    <div className="button-container">
       <p className="button-label">{label}</p>
-    </a>
-  </div>
-  
+    </div>
+  </a>
 );
 
 MainButton.propTypes = {
-  embedId: PropTypes.string.isRequired
+  embedId: PropTypes.string.isRequired,
 };
 
 export default MainButton;
