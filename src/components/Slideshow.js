b8c9a299488951f1759d791ff50a24c46779f0e8
tylanbm <tylanmcpherson@gmail.com>
2022-04-10 10:52:20 -0700

    Now using screenshots from production build

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,58 +1,74 @@
-import React from 'react'
+import React from "react";
 
-const images = ["screen1.png", 'screen2.png', 'screen3.png','screen4.png','screen5.png','screen6.png']
+const images = [
+  "screen1.png",
+  "screen2.png",
+  "screen3.png",
+  "screen4.png",
+  "screen5.png",
+  "screen6.png",
+  "screen7.png",
+  "screen8.png",
+  "screen9.png",
+];
 
 const delay = 4000;
 
 function Slideshow() {
-    const [index, setIndex] = React.useState(0);
-    const timeoutRef = React.useRef(null);
+  const [index, setIndex] = React.useState(0);
+  const timeoutRef = React.useRef(null);
 
-    function resetTimeout() {
-        if (timeoutRef.current) {
-          clearTimeout(timeoutRef.current);
-        }
-      }
-    
-    React.useEffect(() => {
-        resetTimeout();
-        timeoutRef.current = setTimeout(
-          () =>
-            setIndex((prevIndex) =>
-              prevIndex === images.length - 1 ? 0 : prevIndex + 1
-            ),
-          delay
-        );
-    
-        return () => {
-            resetTimeout();
-        };
-      }, [index]);
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
 
-    return (
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
     <div className="slideshow">
-      <div className="slideshow-slider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      <div
+        className="slideshow-slider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
       >
         {images.map((imageName, index) => (
-          <div className="slide" key={index} style={{  }}>
-            <img className='slideshow-image shadow' src={require(`../${imageName}`)} alt="Depicts app and showcases features" />
+          <div className="slide" key={index} style={{}}>
+            <img
+              className="slideshow-image shadow"
+              src={require(`../${imageName}`)}
+              alt="Depicts app and showcases features"
+            />
           </div>
         ))}
       </div>
 
       <div className="slideshowDots">
         {images.map((_, idx) => (
-          <div key={idx} className={`slideshowDot${index === idx ? " active" : ""}`}
-          onClick={() => {
-            setIndex(idx);
-          }}></div>
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
         ))}
       </div>
-
     </div>
-
-    
-  )
+  );
 }
 
-export default Slideshow
+export default Slideshow;
